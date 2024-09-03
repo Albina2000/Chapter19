7b3ac4a0eb706ee166cc48ce3052c5f3d0eca461
Albina Zamirbekova <52452443+Albina2000@users.noreply.github.com>
2024-09-03 13:45:50 -0500

    Add files via upload

---
+++ SubmitOrderTest.java
@@ -0,0 +1,63 @@
+package rahulshettyacademy.SeleniumFrameworkDesign;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import rahulshettyacademy.pageobjects.LandingPage;
+import rahulshettyacademy.pageobjects.ProductCatalogue;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class SubmitOrderTest {
+
+	public static void main(String[] args)throws InterruptedException{
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		LandingPage landingPage = new LandingPage(driver);
+		landingPage.goTo();
+		landingPage.loginApplication("albina.zamirbekova2000@gmail.com", "Albina@2000");
+		
+		String productName = "ZARA COAT 3";
+		
+		ProductCatalogue productCatalogue = new ProductCatalogue(driver);
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+		
+		
+		
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+		
+		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+		Boolean match = cartProducts.stream().anyMatch(cartProduct->cartProduct.getText().equalsIgnoreCase(productName));
+		Assert.assertTrue(match);
+		driver.findElement(By.cssSelector(".totalRow button")).click();
+		
+		Actions a = new Actions(driver);
+		
+		a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "United States").build().perform();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+		
+		driver.findElement(By.xpath("(//button[@type='button'])[1]")).click();
+		driver.findElement(By.cssSelector(".action__submit")).click();
+		
+		String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+		
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+		//System.out.println(confirmMessage);
+	}
+
+}
